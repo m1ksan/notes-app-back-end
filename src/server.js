48db5547300a623f530a54a047afbcaa72d79ff3
machfud ichsan <101936661+m1ksan@users.noreply.github.com>
2022-12-26 12:14:23 +0700

    initial commit

---
+++ src/server.js
@@ -0,0 +1,20 @@
+/* eslint-disable eol-last */
+// eslint-disable-next-line no-console
+const Hapi = require('@hapi/hapi');
+const routes = require('./router');
+
+const init = async () => {
+  const server = Hapi.server({
+    port: 5000,
+    host: 'localhost',
+    routes: {
+      cors: {
+        origin: ['*'],
+      },
+    },
+  });
+  server.route(routes);
+  await server.start();
+  console.log(`Server berjalan pada ${server.info.uri}`);
+};
+init();
